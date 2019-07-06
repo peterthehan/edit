459eba846d4207bf61861bd1335d0e710542d35c
Peter Han <peter.han.9493@gmail.com>
2019-07-06 00:30:25 -0400

    Run prettier

--- background.js
+++ background.js
@@ -1,11 +1,14 @@
 let toggle = true;
 
-chrome.browserAction.onClicked.addListener((_) => {
-	chrome.tabs.executeScript({ file: toggle ? 'enter.js' : 'exit.js', }, (_) => toggle = !toggle);
+chrome.browserAction.onClicked.addListener(_ => {
+  chrome.tabs.executeScript(
+    { file: toggle ? 'enter.js' : 'exit.js' },
+    _ => (toggle = !toggle)
+  );
   chrome.notifications.create({
     type: 'basic',
     iconUrl: 'assets/pencil_48.png',
     title: 'Edit',
-    message: `${toggle ? 'Entering' : 'Exiting'} edit mode.`,
+    message: `${toggle ? 'Entering' : 'Exiting'} edit mode.`
   });
 });
